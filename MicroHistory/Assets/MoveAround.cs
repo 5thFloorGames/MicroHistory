26a690bbb2a71c732b95b88e28b1375418688c90
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-05-22 15:06:15 +0300

    processor melting first prototype done

---
+++ MicroHistory/Assets/MoveAround.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAround : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	private GameObject village;
+	private bool founder = false;
+	private int layerMask = 0;
+
+	void Awake(){
+		village = Resources.Load<GameObject> ("Village");
+	}
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		agent.SetDestination(new Vector3 (Random.Range(-4.5f, 4.5f), 0.55f, Random.Range(-4.5f, 4.5f)));
+		StartCoroutine(NewDestination());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public IEnumerator NewDestination(){
+		while (true) {
+			agent.SetDestination(new Vector3 (Random.Range(-4.5f, 4.5f), 0.55f, Random.Range(-4.5f, 4.5f)));
+			yield return new WaitForSeconds(3f);
+		}
+	}
+
+	public void OnCollisionEnter(Collision col){
+		if (col.collider.tag == "Peasant" && !VillageNearby() && !founder) {
+			Instantiate(village, transform.position, Quaternion.identity);
+			founder = true;
+		}
+	}
+	
+	private bool VillageNearby(){
+		return false;
+	}
+}
---
+++ MicroHistory/Assets/SpawnPeasants.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPeasants : MonoBehaviour {
+
+	private GameObject peasant;
+
+	// Use this for initialization
+	void Start () {
+		peasant = Resources.Load<GameObject>("Peasant");
+		StartCoroutine (Spawn ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Spawn(){
+		while (true) {
+			Instantiate (peasant, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(10f);
+		}
+	}
+}
